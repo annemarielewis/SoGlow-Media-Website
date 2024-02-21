1673620af40d57af91c0964ab06be32e6971308c
Annemarie Lewis <a.m.lewisfilms@gmail.com>
2024-02-20 23:56:07 -0700

    end of day commit

---
+++ server.js
@@ -0,0 +1,15 @@
+//live server logic
+var liveServer = require("live-server");
+var params = {
+    port: 8181,
+    host: "0.0.0.0",
+    root: ".", // Set root directory to current directory
+    open: false,
+    ignore: 'scss,my/templates',
+    file: "index.html",
+    wait: 1000,
+    mount: [['/components', './node_modules']],
+    logLevel: 2,
+    middleware: [function(req, res, next) { next(); }]
+};
+liveServer.start(params);
